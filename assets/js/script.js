916cb41d182d74403f980c635bf3c26222c2062c
Yuleni1 <myuleni4@gmail.com>
2022-05-18 21:39:11 -0400

    added ability to submit custom tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,20 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskFormHandler = function(event) {
     event.preventDefault();
+  
 
 var taskNameInput = document.querySelector("input[name='task-name']").value;
 var taskTypeInput = document.querySelector("select[name='task-type']").value;
-  
-
-  var taskDataObj = {
+var taskDataObj = {
     name: taskNameInput,
     type: taskTypeInput
   };
 
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+  formEl.reset();
   createTaskEl(taskDataObj);
 };
   var createTaskEl = function(taskDataObj) {
